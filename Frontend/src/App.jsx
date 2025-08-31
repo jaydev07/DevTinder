0db146337390acff7081b5d0de06eb9b109f1cb5
jaydev07 <jdbhavsar213@gmail.com>
2025-08-31 23:30:53 +0530

    React-router setup with Outlet

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Body from './components/Body';
+import Login from './components/Login';
+import Profile from './components/Profile';
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />} >
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
